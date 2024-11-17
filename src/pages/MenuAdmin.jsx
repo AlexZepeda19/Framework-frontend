468a61d8341d7b8388f1c9f80b92ef46a1f5310b
Cxrloxs17 <antonio.sandoval22@itca.edu.sv>
2024-11-17 00:03:23 -0600

    Navbar de Admin

--- src/pages/MenuAdmin.jsx
+++ src/pages/MenuAdmin.jsx
@@ -1,10 +1,21 @@
-import React from 'react'; 
+import React from 'react';
+import './MenuAdmin.css'; // Asegúrate de importar el archivo CSS
+
 const MenuAdmin = () => {
-     return ( 
-     <div>
-         <h1 color='black'>Menú de Administración</h1> 
-         <p>Has iniciado sesión y ahora puedes acceder a las funciones administrativas.</p> 
-         </div>
-          );
-          }; 
-         export default MenuAdmin
+    return (
+        <div className="menu-container">
+            <nav className="navbar">
+                <ul>
+                    <li><a href="/home">Inicio</a></li>
+                    <li><a href="/users">Usuarios</a></li>
+                    <li><a href="/settings">Configuraciones</a></li>
+                    <li><a href="/logout">Cerrar sesión</a></li>
+                </ul>
+            </nav>
+            <h1>Menú de Administración</h1>
+            <p>Has iniciado sesión y ahora puedes acceder a las funciones administrativas.</p>
+        </div>
+    );
+};
+
+export default MenuAdmin;
