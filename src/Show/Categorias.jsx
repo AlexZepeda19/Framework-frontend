5d05d6d3095f4289355244c4fb7cff81584a5067
Cxrloxs17 <antonio.sandoval22@itca.edu.sv>
2024-11-18 00:29:41 -0600

    btnregresar

--- src/Show/Categorias.jsx
+++ src/Show/Categorias.jsx
@@ -209,6 +209,16 @@ const Categorias = () => {
           </div>
         </div>
       )}
+
+      {/* Botón para regresar */}
+      <div className="d-flex justify-content-center mt-4">
+        <button
+          className="btn btn-primary"
+          onClick={() => window.history.back()}
+        >
+          Regresar
+        </button>
+      </div>
     </div>
   );
 };
